2fea5acf16baa265fa95ed84709f29c517709dca
Manuel <magpags@gmail.com>
2020-02-16 15:41:13 -0600

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 //Rutas
 import { APP_ROUTES } from './app.routes';
 
-// import { AppRoutingModule } from './app-routing.module';
+// Módulos 
+import { PagesModule } from "./pages/pages.module";
+// import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from "./login/register.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumsComponent,
-    PagesComponent,
+    LoginComponent,      
     RegisterComponent,
   ],
   imports: [
-    BrowserModule,    
+    BrowserModule,  
+    PagesModule,  
+    // SharedModule,
     APP_ROUTES
   ],
   providers: [],
